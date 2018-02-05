36c536437221121c369f3015b91da9aa223dae14
huangyuan9 <838600146@qq.com>
2018-02-05 22:07:10 +0800

    hebing

--- utils/constant.js
+++ utils/constant.js
@@ -31,7 +31,7 @@ var constant = {
   userInfoKey: 'userInfoKey',
   level: '普通用户',
   // 用户token
-  userToken: 'veygath3xf48ggth4eh6jmp1p60v7mnv',
+  userToken: 'pev0it3yhomg0qhva68tevqf2ydw98aq',
   quote_id: 'quote_id', // 购物车id
   qty: 'qty'   // 购物车商品数量
 }
